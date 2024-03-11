82fb4834be4b8f12d97a67dc4354ead94f60beb7
AntonSizov <sizov.anton.v@gmail.com>
2024-03-12 00:10:24 +0200

    final

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,15 @@
-let some:unknown;
-some = 'Text';
-let str: string;
-str = some;
+// let some:unknown;
+// some = 'Text';
+// let str: string;
+// str = some;
+
+function getSome() {
+  return 'Text';
+}
+
+let someData: unknown = getSome(); 
+if (typeof someData === 'string') {
+  // console.log(someData); 
+}
 
 export {};
